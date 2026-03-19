01a1758dbee67e50ba87a196bba3cad65c682ef8
Andrei Briceag <andrei98briceag@gmail.com>
2026-03-19 14:54:29 +0100

    E1 lista cliente con numero ordine e lista prodotti

--- src/main/java/AndreiB/Application.java
+++ src/main/java/AndreiB/Application.java
@@ -1,8 +1,42 @@
 package AndreiB;
 
+import entities.Customer;
+import entities.Order;
+import entities.Product;
+
+import java.util.List;
+import java.util.Map;
+
+import static helpersStream.es1.raggruppaOrdiniPerCliente;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("=== ESERCIZIO 1 ===\n");
+        System.out.println(" Mappa clienti con numeri dei loro ordini\n");
+
+        Map<Customer, List<Order>> ordiniPerCliente = raggruppaOrdiniPerCliente();
+
+        for (Customer cliente : ordiniPerCliente.keySet()) {
+            List<Order> ordini = ordiniPerCliente.get(cliente);
+
+            System.out.println(cliente.getName());
+            System.out.println("Numero ordini: " + ordini.size());
+
+            if (ordini.isEmpty()) {
+                System.out.println(" Nessun ordine effettuato");
+            } else {
+                for (Order o : ordini) {
+                    System.out.println(" Ordine del " + o.getOrderDate());
+                    System.out.println("  Prodotti:");
+                    for (Product p : o.getProducts()) {
+                        System.out.print(p.getName() + ". ");
+                    }
+                }
+            }
+            System.out.println("\n");
+
+        }
     }
 }
--- src/main/java/helpersStream/es1.java
+++ src/main/java/helpersStream/es1.java
@@ -1,5 +1,17 @@
 package helpersStream;
 
+import data.SampleData;
+import entities.Customer;
+import entities.Order;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class es1 {
-    
+
+    public static Map<Customer, List<Order>> raggruppaOrdiniPerCliente() {
+        return SampleData.ORDERS.stream()
+                .collect(Collectors.groupingBy(Order::getCustomer));
+    }
 }
